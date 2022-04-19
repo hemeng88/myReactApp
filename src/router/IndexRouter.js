dca1a8efcf34e41d92040e49e7739cffd78f9f40
hemeng88 <kamou981009@gmail.com>
2022-04-19 17:55:24 +0800

    test

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -7,8 +7,8 @@ import {Routes} from "react-router";
 export default function IndexRouter() {
     return (<HashRouter>
         <Routes>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/" element={<NewsSandBox />}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="*" element={<NewsSandBox />}></Route>
         </Routes>
     </HashRouter>)
 }
--- src/views/sandbox/NewsSandBox.js
+++ src/views/sandbox/NewsSandBox.js
@@ -1,7 +1,25 @@
 import React from "react"
+import SideMenu from "../../components/sandbox/SideMenu";
+import TopHeader from "../../components/sandbox/TopHeader";
+import {Navigate, Routes} from "react-router";
+import {Route} from "react-router-dom";
+import Home from "./home/Home";
+import UserList from "./user-manage/UserList";
+import RoleList from "./right-manage/RoleList";
+import RightList from "./right-manage/RightList";
 
 export default function NewsSandBox() {
     return (<div>
-        NewSandBox
+        <SideMenu></SideMenu>
+        <TopHeader></TopHeader>
+
+        <Routes>
+            <Route path={"/home"} element={<Home/>}/>
+            <Route path={"/user-manage/list"} element={<UserList/>}/>
+            <Route path={"/right-manage/role/list"} element={<RoleList/>}/>
+            <Route path={"/right-manage/right/list"} element={<RightList/>}/>
+
+            <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
     </div>)
 }
