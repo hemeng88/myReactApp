f678d8b103b96d306983d77826b10d2a5874dd34
hemeng88 <kamou981009@gmail.com>
2022-04-22 14:23:33 +0800

    test

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -2,13 +2,12 @@ import React, {useEffect, useState} from 'react'
 import Sider from "antd/es/layout/Sider";
 import {Menu} from "antd";
 import './index.css'
-import {useNavigate} from "react-router";
+import {useLocation, useNavigate} from "react-router";
 import axios from "axios";
 import {CheckOutlined, HomeOutlined, KeyOutlined, MessageOutlined, SendOutlined, UserOutlined} from "@ant-design/icons";
 
 export default function SideMenu() {
     const navigate = useNavigate()
-
     const [menu, setMenu] = useState([]);
     useEffect(() => {
         axios.get("http://localhost:3004/rights?_embed=children").then(res => {
@@ -27,7 +26,6 @@ export default function SideMenu() {
 
     Object.keys(menu).forEach(key => {
         menu[key].icon = iconList[menu[key].key]
-        console.log(menu[key].key)
         menu[key].label = menu[key].title
         if (menu[key].children && menu[key].children.length > 0) {
             menu[key].children.forEach(key => {
@@ -49,18 +47,23 @@ export default function SideMenu() {
             return false
         });
     }
+    const location = useLocation()
 
     return (<Sider trigger={null} collapsible collapsed={false}>
-        <div className="logo">全球新闻发布管理系统</div>
-        <Menu
-            onClick={(e) => {
-                navigate(e.key)
-            }}
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['1']}
-            mode="inline"
-            theme="dark"
-            items={filterList(menu)}
-        />
+        <div className="sidebar">
+            <div className="logo">全球新闻发布管理系统</div>
+            <div className="menu">
+            <Menu
+                onClick={(e) => {
+                    navigate(e.key)
+                }}
+                selectedKeys={[location.pathname]}
+                openKeys={["/"+location.pathname.split("/")[1]]}
+                mode="inline"
+                theme="dark"
+                items={filterList(menu)}
+            />
+            </div>
+        </div>
     </Sider>)
 }
