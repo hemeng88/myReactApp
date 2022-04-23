979f8ecf1a9cd8ef2de439a2e88e63786c6a3a4e
hemeng88 <kamou981009@gmail.com>
2022-04-23 20:01:42 +0800

    test

--- src/views/sandbox/right-manage/RoleList.js
+++ src/views/sandbox/right-manage/RoleList.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Button, Table} from "antd";
+import axios from "axios";
+import {DeleteOutlined, UnorderedListOutlined} from "@ant-design/icons";
 
 export default function RoleList() {
+    const [dataSource, setdataSource] = useState([])
+    const columns = [{
+        title: 'ID', dataIndex: 'id', render: (id) => {
+            return <b>{id}</b>
+        },
+    }, {
+        title: '角色名称', dataIndex: 'roleName'
+    }, {
+        title: '操作', render: (row) => {
+            return <div>
+                <Button shape="circle" danger icon={<DeleteOutlined/>}></Button>
+                <Button shape="circle" type="primary" icon={<UnorderedListOutlined/>}></Button>
+            </div>
+        }
+    }]
+    useEffect(() => {
+        axios.get("http://localhost:3004/roles").then(res => {
+            setdataSource(res.data)
+        })
+    }, [])
+
     return (<div>
-        RoleList
+        <Table dataSource={dataSource} columns={columns} rowKey={(row) => row.id}></Table>
     </div>)
 }
